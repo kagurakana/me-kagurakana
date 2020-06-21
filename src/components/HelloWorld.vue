0f81b2b148240fda2801930d43e2fab8e384b83a
kagurakana <1278820830@qq.com>
2020-06-21 13:06:51 +0800

    init:init env

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="fullpage">
+    <div class="section active">Some section</div>
+    <div class="section">Some section</div>
+    <div class="section">
+      <div class="slide">Slide 1</div>
+      <div class="slide">Slide 2</div>
+      <div class="slide">Slide 3</div>
+      <div class="slide">Slide 4</div>
+    </div>
+    <div class="section">Some section</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  mounted() {
+    $("#fullpage").fullpage();
+  },
+  data() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
